e829eb520daa2bd5f54b3dcd4d90ee09ce40844f
Ola <aleksandra.jach02@gmail.com>
2024-02-03 22:45:23 +0100

    add day and night cycle

---
+++ Assets/Scripts/DayTimeController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DayTimeController : MonoBehaviour
+{
+    const float secondsInDay = 86400f;
+
+    [SerializeField] Color nightLightColor;
+    [SerializeField] AnimationCurve nightTimeCurve;
+    [SerializeField] Color dayLightColor = Color.white;
+
+   float time;
+
+    [SerializeField] float timeScale = 60f;
+
+   [SerializeField] Text text;
+   [SerializeField] UnityEngine.Rendering.Universal.Light2D globalLight;
+   private int days;
+
+   float Hours
+    {
+         get
+         {
+              return time / 3600f;
+         }
+    }
+
+    float Minutes
+    {
+        get
+        {
+            return time % 3600f / 60f;
+        }
+    }
+
+   private void Update()
+   {
+       time += Time.deltaTime * timeScale;
+       int hh = (int)Hours;
+       int mm = (int)Minutes;
+       text.text = hh.ToString("00") + ":" + mm.ToString("00");
+       float v = nightTimeCurve.Evaluate(Hours);
+       Color c = Color.Lerp(dayLightColor, nightLightColor, v);
+       globalLight.color = c;
+       if (time > secondsInDay)
+       {
+           NextDay();
+       }
+   }
+
+   private void NextDay()
+   {
+       time = 0;
+       days += 1;
+   }
+
+}
